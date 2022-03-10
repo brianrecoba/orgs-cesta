0c287d3eb3ca1b1408b6ccc18585a1f784ce8c3e
Your Name <you@example.com>
2022-03-10 16:30:44 -0300

    Projeto pronto

---
+++ src/telas/Cesta/components/Item.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Texto from '../../../components/Texto'
+import {View, Image, StyleSheet, FlatList} from 'react-native'
+
+export default function Item({item: {nome, imagem} }) {
+
+     return<View style={estilos.item} >
+    <Image style={estilos.imagem} source={imagem}/>
+    <Texto style={estilos.nome}>{nome}</Texto>
+    </View>
+
+}
+
+const estilos = StyleSheet.create({
+    
+    item: {
+        flexDirection: "row",
+        borderBottomWidth: 1, 
+        borderBottomColor: "#ECECEC",
+        paddingVertical: 16,
+        marginHorizontal: 16,
+        alignItems: "center"
+    },
+    imagem: {
+        width: 46,
+        height: 46
+    },
+    nome :{
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 11,
+        color: "#464646"
+    }
+
+})
